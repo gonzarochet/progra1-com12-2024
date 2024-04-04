d9b45fc6c6488b64774f24f060a8678c9873fbf1
gonzarochet <rochetgonzalo@gmail.com>
2024-04-04 18:10:55 -0300

    carga, mostrar, de arreglo a pila

--- 27-03-2024-Arreglos/main.c
+++ 27-03-2024-Arreglos/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
+#include "pila.h"
 
 #define DIM2 10
-void mostrarArreglo(int a[], int v);
-int cargaRandomArregloVacio(int arreglo[], int dimension, int cantidadElementos);
 
+int cargaRandomArregloVacio(int arreglo[], int dimension, int cantidadElementos);
+int cargaArreglo(int arreglo[],int v, int dim);
+void cargaArregloReferencia(int arreglo[],int * v, int dim);
+void mostrarArreglo(int a[], int v);
+void arregloToPila(int arreglo[], int validos, Pila * p);
+int cuentaDatosPila(Pila p);
 
 const int DIM = 10;
 
@@ -26,11 +32,11 @@ int main()
 //
 //    validosNotas = i;
 
-    validosNotas = cargaRandomArregloVacio(arregloNotas,DIM,15);
-    mostrarArreglo(arregloNotas,15);
+    cargaArregloReferencia(arregloNotas,&validosNotas,DIM);
+    mostrarArreglo(arregloNotas,validosNotas);
 
 
-     /*
+    /*
     int arreglo  [20] = {0};
     int arreglo2 [DIM];
 
@@ -44,33 +50,73 @@ int main()
     return 0;
 }
 
+
+/// 1.
+int cargaArreglo(int arreglo[],int v, int dim)
+{
+
+    char option;
+    int dato;
+
+    while(v<dim && option != 27)
+    {
+        printf("Ingrese un dato\n");
+        scanf("%d", &arreglo[v]);
+        v++;
+
+        printf("Quiere seguir cargando datos? Presione ESC para salir\n");
+        option = getch();
+        system("cls");
+    }
+
+    return v;
+
+}
+
+
+void cargaArregloReferencia(int arreglo[],int * v, int dim)
+{
+
+    char option;
+    int dato;
+    int i = *v;
+
+    while(i<dim && option != 27)
+    {
+        printf("Ingrese un dato\n");
+        scanf("%d", &arreglo[i]);
+        i++;
+
+        printf("Quiere seguir cargando datos? Presione ESC para salir\n");
+        option = getch();
+        system("cls");
+    }
+
+  *v = i;
+
+}
+ /// Esta funcion cada vez que se llama comienza a cargar elementos al arreglo desde la posicion 0 (cero)
 int cargaRandomArregloVacio(int arreglo[], int dimension, int cantidadElementos)
 {
 
     int validos = 0;
     int i = 0;
 
-    //if(cantidadElementos <dimension){
-        for(i = 0; i< cantidadElementos; i++ ){
-            arreglo[i] = rand()%10;
-        }
+    for(i = 0; i< cantidadElementos; i++ )
+    {
+        arreglo[i] = rand()%10+1;
+    }
 
-        validos = i;
+    validos = i;
     //}
 
     return validos;
 }
 
-
-
-
-
-
-
-
-
+/// 2.
 void mostrarArreglo(int a[], int v)
 {
+    printf("[");
     for(int i = 0; i<v; i++)
     {
         if(i % 10 == 0)
@@ -79,4 +125,50 @@ void mostrarArreglo(int a[], int v)
         }
         printf("%d |", a[i]);
     }
+    printf("]");
+}
+
+
+
+/***
+* 4. Hacer una función que reciba como parámetro un arreglo, la cantidad de elementos (válidos)
+* cargados en él y una Pila. La función debe copiar los elementos del arreglo en la pila.
+***/
+
+void arregloToPila(int arreglo[], int validos, Pila * p)
+{
+    int cantPilas = cuentaDatosPila(*p);
+
+    int posArreglo = 0;
+
+    while(posArreglo < validos && cantPilas < 50){
+
+        apilar(p,arreglo[posArreglo]);
+        cantPilas++;
+        posArreglo++;
+    }
+}
+
+
+int pilaToArreglo(Pila *p, int arreglo[], int v, int dim) {}
+
+
+/// FUNCIONES EXTRA
+/// Funcion que carga elementos en un rango determinado
+/// Funciones para cargar un arreglo sin numeros repetidos.
+
+
+int cuentaDatosPila(Pila p){
+
+    Pila aux;
+    inicpila(&aux);
+    int contador = 0;
+
+    while(!pilavacia(&p)){
+        apilar(&aux,desapilar(&p));
+        contador ++;
+    }
+
+    return contador;
+
 }
